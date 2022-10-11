6c850eabc16c6484989627b18c355c34292ef81e
polvinh0 <ieiel_4@hotmail.com>
2022-10-11 13:32:36 -0300

    Arrays

--- src/main/java/CalculadoraIMC.java
+++ src/main/java/CalculadoraIMC.java
@@ -26,5 +26,59 @@ public class CalculadoraIMC {
         System.out.println("Este é o seu IMC = " + imc);
         System.out.printf("este é o seu IMC = %.2f\n", imc);
         System.out.printf("Ola, meu nome é %s %s e tenho %d anos\n", nome, nome2, idade);
+
+        //https://www.calculoimc.com.br/
+        if (imc <= 16){
+            System.out.println("Muito abaixo do peso");
+        } else if (imc>=17 && imc <=18.49){
+            System.out.println("Abaixo do peso");
+        }else if (imc>=18.5 && imc <=24.99) {
+            System.out.println("Peso Normal");
+        } else if (imc>=25 && imc <=29.99) {
+            System.out.println("Acima do peso");
+        } else if (imc>=30 && imc <=34.99) {
+            System.out.println("Obesidade 1");
+        } else if (imc>=35 && imc <=39.99) {
+            System.out.println("Obesidade 2");
+        }else {
+            System.out.println("Obesidade 3");
+        }
+
+
+        //Switch-case
+        System.out.println("Selecione uma dieta (1 a 3)");
+        System.out.println("1 - Agua");
+        System.out.println("2 - Fruta");
+        System.out.println("3 - Batata");
+        int dieta = entrada.nextInt();
+
+        switch (dieta){
+            case 1:
+                System.out.println("Você escolheu a dieta da água!");
+                break;//impedir de testar as outras condições
+            case 2:
+                System.out.println("Você escolheu a dieta da fruta!");
+                break;
+            case 3:
+                System.out.println("Você escolheu a dieta da batata!");
+                break;
+            default://""else""
+                System.out.println("esta dieta não existe!");
+        }
+
+
+        System.out.println("você tem quantos alimentos preferidos?");
+        int totalAlimentos = entrada.nextInt();
+
+        for (int i = 0; i < totalAlimentos; i++){
+            String alimento = entrada.nextLine();
+            System.out.println("Você gosta de " + alimento);
+        }
+
+        int i = 0;
+            while(i < totalAlimentos) {
+            System.out.println(i);
+            i++;
+        }
     }
 }
---
+++ src/main/java/EstudoArrays.java
@@ -0,0 +1,50 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class EstudoArrays {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5};//possui tamanho fixo
+        System.out.println(numeros.length);// tamanho do array
+        numeros[3] = 1000;
+        System.out.println(numeros[3]);
+        for (int i = 0; i < numeros.length;i++){
+            System.out.println("A posição " + i + " com valor de " + numeros[i]);
+        }
+        int[] numeros2 = new int [50];
+        numeros2[0] = 500;
+        numeros2[1] = -200;
+        //...
+        numeros2[49] = 1;
+
+        double[] notas = new double[5];
+        String[] nomes= {"jose", "renato", "victor"};
+        String[] sobrenomes = new String[10];
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um numero");
+//        int quantidade
+
+
+        System.out.println("Digite o total de notas: ");
+        int totalNotas = entrada.nextInt();//5
+
+
+        double[] notasProva = new double[totalNotas];
+
+
+        for (int i = 0; i < totalNotas;i++) {
+            System.out.println("digite o valor da " + (i+1) + "ª nota: ");
+            notasProva[i] = entrada.nextDouble();
+        }
+        double soma = 0.0;
+        for(double nota: notasProva) {
+            soma += nota;
+        }
+        double media = soma/totalNotas;
+        System.out.println(media);
+//        condição ternaria
+        String mensagem = (media <7 ) ? "vocÊ está reprovado" : "você está aprovado";
+        System.out.println(mensagem);
+    }
+}
